6fbe049738bd4a4ce8f6e29e4818e1a93ed95111
kablo aslan <aslan.kablo@mail.ru>
2021-08-28 22:10:41 +0300

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+   return (
+      <main>
+         <PageHero title="about" />
+         <Wrapper className="page section section-center">
+            <img src={aboutImg} alt="nice desk" />
+            <article>
+               <div className="title">
+                  <h2>our story</h2>
+                  <div className="underline"></div>
+               </div>
+               <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Tenetur non repudiandae consequuntur optio accusamus eaque
+                  iure distinctio sunt eius minus ipsum vero dolorem aliquid
+                  quisquam quia reprehenderit modi a reiciendis nemo libero
+                  veritatis, blanditiis corrupti odit cupiditate! Tempora et
+                  nobis quo, quisquam maiores fuga iure eius? Non sequi
+                  exercitationem quod.
+               </p>
+            </article>
+         </Wrapper>
+      </main>
+   );
+};
+
+const Wrapper = styled.section`
+   display: grid;
+   gap: 4rem;
+   img {
+      width: 100%;
+      display: block;
+      border-radius: var(--radius);
+      height: 500px;
+      object-fit: cover;
+   }
+   p {
+      line-height: 2;
+      max-width: 45em;
+      margin: 0 auto;
+      margin-top: 2rem;
+      color: var(--clr-grey-5);
+   }
+   .title {
+      text-align: left;
+   }
+   .underline {
+      margin-left: 0;
+   }
+   @media (min-width: 992px) {
+      grid-template-columns: 1fr 1fr;
+   }
+`;
+export default AboutPage;
